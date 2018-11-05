8db9785f9aa196a185ebc5130c9538d078a42f16
mahi-a <mahesh@westagilelabs.com>
2018-11-05 17:32:27 +0530

    Fixing vulnerabilities

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#show'
 
-  resources :users, only: [:index]
+  resources :users, only: [:index], defaults: { format: 'json' }
 end
